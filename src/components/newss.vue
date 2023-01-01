add2399ce538209970896d5fb980984534418618
kocaoglu.ismail@gmail.com <kocaoglu.ismail@gmail.com>
2023-01-01 07:55:38 +0300

    first release

---
+++ src/components/newss.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+defineProps({
+  ggb: String,
+});
+const TEST = ref(0);
+</script>
+
+<template>
+  <h1>{{ ggb }}</h1>
+
+  <div class="card">
+    <button type="button" @click="TEST++">TEST is {{ TEST }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>

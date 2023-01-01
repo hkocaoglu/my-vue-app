c23ebe89f981e82ea39e4b80b4010d3a045f3a5e
kocaoglu.ismail@gmail.com <kocaoglu.ismail@gmail.com>
2023-01-01 07:53:01 +0300

    First Release

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import newss from "./components/newss.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <newss ggb="Vite + Vue" />
+</template>
+
+<style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #8b64ffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+</style>
